dbc286ad9a7953768408fe1e5a5603b1b966a4f4
tilliwilli <tilliwilli@gmail.com>
2020-09-17 11:57:31 +0300

    +

---
+++ Test/TemplateDataInjectorService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+
+namespace Test
+{
+    public class TemplateDataInjectorService
+    {
+        public void InjectData(
+            XSSFWorkbook workbook,
+            Dictionary<string, WidgetData> dataSet
+        )
+        {
+            var markerInfos = Pass1_ExtractMarkers(workbook);
+            var dataInjections = markerInfos.Select(x => new DataInjection
+            {
+                MarkerInfo = x,
+                WidgetData = dataSet[x.MarkerId]
+            }).ToList();
+            Pass2_InjectData(workbook, dataInjections);
+        }
+
+        private List<MarkerInfo> Pass1_ExtractMarkers(
+            XSSFWorkbook workbook
+        )
+        {
+            var result = new List<MarkerInfo>();
+            Enumerable.Range(0, workbook.NumberOfSheets).ToList()
+                .ForEach(sheetIndex =>
+                {
+                    var sheet = workbook.GetSheetAt(sheetIndex);
+                    Console.WriteLine($"|sheet SheetName={sheet.SheetName} FirstRowNum={sheet.FirstRowNum} LastRowNum={sheet.LastRowNum} PhysicalNumberOfRows={sheet.PhysicalNumberOfRows}|");
+
+                    for (var rowIndex = sheet.FirstRowNum; rowIndex <= sheet.LastRowNum; ++rowIndex)
+                    {
+                        var row = sheet.GetRow(rowIndex);
+                        if (row == null)
+                        {
+                            Console.WriteLine($"\t[row {rowIndex} isEmpty]");
+                            continue;
+                        }
+                        Console.WriteLine($"\t[row {rowIndex} FirstCellNum={row.FirstCellNum} LastCellNum={row.LastCellNum} PhysicalNumberOfCells={row.PhysicalNumberOfCells}]");
+
+                        for (var cellIndex = row.FirstCellNum; cellIndex < row.LastCellNum; ++cellIndex)
+                        {
+                            var cell = row.GetCell(cellIndex);
+                            Console.Write($"\t\t<cell {cell.RowIndex}.{cell.ColumnIndex} type={cell.CellType} value={cell.ToString()}> ");
+                            if (IsMarkedCell(cell))
+                                result.Add(new MarkerInfo { SheetIndex = sheetIndex, RowIndex = rowIndex, CellIndex = cellIndex, MarkerId = ExtractMarkerValue(cell) });
+                        }
+                        Console.WriteLine();
+                    }
+                });
+            Console.WriteLine();
+            return result;
+        }
+
+        private bool IsMarkedCell(ICell cell)
+        {
+            if (cell.CellType == CellType.String)
+            {
+                var stringCellValue = cell.StringCellValue.Trim();
+                if (stringCellValue.Length < 4)
+                    return false;
+                if (stringCellValue.Substring(0, 2) == "{{" && stringCellValue.Substring(stringCellValue.Length - 2, 2) == "}}")
+                    return true;
+            }
+            return false;
+        }
+
+        private string ExtractMarkerValue(ICell cell)
+        {
+            return cell.StringCellValue.Trim().Substring(2, cell.StringCellValue.Length - 4);
+        }
+
+
+
+        private void Pass2_InjectData(
+            XSSFWorkbook workbook,
+            List<DataInjection> dataInjections
+        )
+        {
+            dataInjections.ForEach(dataInjection => InsertCellRange(workbook, dataInjection.MarkerInfo, dataInjection.WidgetData.Values));
+        }
+
+
+        private void InsertCellRange(XSSFWorkbook workbook, MarkerInfo markerInfo, List<List<string>> dataSet)
+        {
+            var dataSetRowCount = dataSet.Count;
+            var dataSetColCount = dataSet.Count == 0
+                ? 0
+                : dataSet[0].Count;
+
+            var insertionStartRowIndex = markerInfo.RowIndex;
+            var insertionStartCellIndex = markerInfo.CellIndex;
+
+            var sheet = workbook.GetSheetAt(markerInfo.SheetIndex);
+            for (var dataRowIndex = 0; dataRowIndex < dataSetRowCount; ++dataRowIndex)
+            {
+                var dataRow = dataSet[dataRowIndex];
+                var currentRowIndex = insertionStartRowIndex + dataRowIndex;
+                var currentRow = sheet.GetRow(currentRowIndex);
+
+                if (currentRow == null)
+                    currentRow = sheet.CreateRow(currentRowIndex);
+
+                for (var dataColIndex = 0; dataColIndex < dataSetColCount; ++dataColIndex)
+                {
+                    var dataValue = dataRow[dataColIndex];
+                    var currentCellIndex = insertionStartCellIndex + dataColIndex;
+                    var currentCell = currentRow.GetCell(currentCellIndex);
+
+                    if (currentCell == null)
+                        currentCell = currentRow.CreateCell(currentCellIndex);
+
+                    currentCell.SetCellType(CellType.String);
+                    currentCell.SetCellValue(dataValue);
+                }
+            }
+        }
+    }
+
+
+    public class MarkerInfo
+    {
+        public string MarkerId { get; set; }
+        public int SheetIndex { get; set; }
+        public int RowIndex { get; set; }
+        public int CellIndex { get; set; }
+    }
+
+    public class DataInjection
+    {
+        public MarkerInfo MarkerInfo { get; set; }
+        public WidgetData WidgetData { get; set; }
+        public InjectionOptions InjectionOptions { get; set; }
+    }
+
+    public class InjectionOptions
+    {
+
+    }
+}
--- XlsxTemplateReporter/Program.cs
+++ XlsxTemplateReporter/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using NPOI.XSSF.UserModel;
+using Test;
 
 namespace XlsxTemplateReporter
 {
@@ -6,7 +12,77 @@ namespace XlsxTemplateReporter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var templates = new[]
+            {
+                "template1",
+                "template2",
+                "template3",
+            };
+            var files = templates.Select(x => new { 
+                In = $"./Templates/{x}.xlsx",
+                Out = $"./Output/{x}.out.xlsx"
+            });
+
+            files.ToList().ForEach(file =>
+            {
+                using var fileStream = File.Open(file.In, FileMode.Open, FileAccess.ReadWrite);
+                var workbook = new XSSFWorkbook(fileStream);
+                var service = new TemplateDataInjectorService();
+                service.InjectData(workbook, PrepareData());
+
+                using (var outputFileStream = File.Open(file.Out, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    workbook.Write(outputFileStream);
+                }
+            });
+            
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static Dictionary<string, WidgetData> PrepareData()
+        {
+            var files = new[]
+            {
+                "table1",
+            };
+
+            var widgets = files
+                .ToList()
+                .Select(file => new
+                {
+                    Name = file,
+                    Data = JsonConvert.DeserializeObject<WidgetData>(File.ReadAllText($"./Data/{file}.json"))
+                })
+                .Select(x=> {
+                    x.Data.Values = Invert(x.Data.Values);
+                    return x;
+                });
+
+            var result = widgets.ToDictionary(x => x.Name, x => x.Data);
+            return result;
+        }
+
+        static List<List<T>> Invert<T>(List<List<T>> array)
+        {
+            var result = new List<List<T>>();
+
+            if (array.Count == 0)
+                return result;
+
+            var rowCount = array.Count;
+            var colCount = array[0].Count;
+
+            for (var col = 0; col < colCount; ++col)
+            {
+                result.Add(new List<T>());
+                for(var row=0;row<rowCount;++row)
+                {
+                    result[col].Add(array[row][col]);
+                }
+            }
+
+            return result;
         }
     }
 }
